02be4407349a05d8212afa770f5d7810163de1b7
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-02 13:47:17 -0300

    Resuelto: Adivina el Número 2
    Resuelto el ejercicio "Adivina el Número 2"

--- TP - Curso Ingreso JS/8-TPs/jsAdivinaElNumero2.js
+++ TP - Curso Ingreso JS/8-TPs/jsAdivinaElNumero2.js
@@ -9,20 +9,58 @@
 Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
-var numeroSecreto; 
-var contadorIntentos;
+//Creo el app
+var miAplicacion = angular.module('AdivinaElNumero2', []);
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
+//Creo el controller
+miAplicacion.controller('ControladorADV2', function($scope){
 
-}
+    $scope.isNumber = angular.isNumber;
+    
+    $scope.Comenzar = function(){
+        $scope.numeroSecreto = Math.floor((Math.random() * 100) + 1);
+        console.log($scope.numeroSecreto);
+        $scope.numeroIngresado = "";
+        $scope.intentos = "";
+    };
 
-function verificar()
-{
-	
-	
-
-}
+    $scope.Verificar = function(){
+        $scope.intentos ++;
+        console.log($scope.intentos);
+        
+        if($scope.numeroIngresado == $scope.numeroSecreto){
+            //alert("Usted es un ganador!!! y en solo " + $scope.intentos + " intentos");
+            switch($scope.intentos){
+                case 1:
+                    alert("usted es un Psíquico");
+                    break;
+                case 2:
+                    alert("excelente percepción");
+                    break;
+                case 3:
+                    alert("Esto es suerte");
+                    break;
+                case 4:
+                    alert("Excelente técnica");
+                    break;
+                case 5:
+                    alert("usted está en la media");
+                    break;
+                default:
+                    alert("falta técnica");
+                    break;
+            }
+            $scope.Comenzar();
+        }
+        else if($scope.numeroIngresado > $scope.numeroSecreto){
+            alert("se pasó…");
+            $scope.numeroIngresado = "";
+        }
+        else{
+            alert("falta…");
+            $scope.numeroIngresado = "";
+        }
+    };
+    
+    window.document.onload = $scope.Comenzar();
+});
