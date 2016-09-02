5089c01f3ed20c6bfef2ddb6c8c90d8dfaf6b178
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-02 01:15:57 -0300

    Resuelto: Ferrete Construcción
    Resuelto el ejercicio "Ferrete Construcción"

--- TP - Curso Ingreso JS/8-TPs/jsFerreteConstruccion.js
+++ TP - Curso Ingreso JS/8-TPs/jsFerreteConstruccion.js
@@ -4,15 +4,28 @@ B.	Mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+var miAplicacion = angular.module('FerreteApp', []);
 
-}
-function Circulo () 
-{
-	
-}
-function Materiales () 
-{
-	
-}
+miAplicacion.controller('FerreteControlador', function($scope){
+    $scope.largo;
+    $scope.ancho;
+    $scope.radio;
+    
+    $scope.Rectangulo = function(){
+        $scope.resultado = $scope.largo * 2 + $scope.ancho * 2;
+        $scope.resultado *= 3;
+        alert($scope.resultado);
+    };
+    
+    $scope.Circulo = function(){
+        $scope.resultado = 2 * Math.PI * parseFloat($scope.radio);
+        $scope.resultado *= 3;
+        alert(Math.ceil($scope.resultado));
+    };
+    
+    $scope.Materiales = function(){
+        $scope.resultado = Math.ceil($scope.largo * $scope.ancho);
+        alert("Se necesitan " + 2*$scope.resultado + " bolsas de cemento y " +  3*$scope.resultado + " de cal");
+    };
+    
+});
